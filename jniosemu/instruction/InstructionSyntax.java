0744b5ad0ab5122df00c40fe6e107c737b0a695e
emil <emil@hesslow.se>
2007-05-09 10:32:52 +0000

    forgot one file

---
+++ jniosemu/instruction/InstructionSyntax.java
@@ -0,0 +1,36 @@
+package jniosemu.instruction;
+
+public abstract class InstructionSyntax implements Comparable
+{
+	/**
+	 * Categories for instructions used for instruction insert menu.
+	 */
+	public static enum CATEGORY {
+		ARITHMETIC_LOGICAL,
+		MOVE,
+		COMPARISON,
+		SHIFT_ROTATE,
+		PROGRAM_CONTROL,
+		DATA_TRANSFER,
+		OTHER
+	}
+	
+	protected CATEGORY category = null;
+
+	/**
+	 * Name of instruction.
+	 */
+	protected String name;
+
+	public abstract String getName();
+
+	public CATEGORY getCategory() {
+		return this.category;
+	}
+
+	public abstract String getArguments() throws InstructionException;
+
+	public int compareTo(Object obj) {
+		return this.name.compareTo(((InstructionSyntax)obj).getName());
+	}
+}
